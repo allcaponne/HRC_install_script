75f4166210115bbaab760d0cad7013ca9c7be660
allcaponne <petuhov.sp@gmail.com>
2018-06-11 23:29:39 +0300

    Add files via upload

--- HRC.sh
+++ HRC.sh
@@ -32,7 +32,7 @@ function compile_node() {
   COIN_ZIP=$(echo $COIN_REPO | awk -F'/' '{print $NF}')
   tar xvzf $COIN_ZIP --strip 1 >/dev/null 2>&1
   compile_error
-  cp bin/polis* /usr/local/bin
+  cp bin/hashrentalcoin* /usr/local/bin
   compile_error
   strip $COIN_DAEMON $COIN_CLI
   cd - >/dev/null 2>&1
